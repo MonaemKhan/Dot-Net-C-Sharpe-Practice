a86c6ef4f839b2e0dc5131c4f663e37f42b09700
MonaemKhan <khanmonaem07@gmail.com>
2023-06-15 03:11:15 +0600

    dot net

---
+++ OOP_Practice/OOP_Practice/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Practice
+{
+    public class Course
+    {
+        public string CourseId { get; set; }
+        public string CourseTitle { get; set; }
+        public string CourseDuration { get; set; }
+        public double CourseCradite { get; set; }
+    }
+}
