54a2e72d24b7d96e9bf73ff80e5e6c8ca07142fc
MonaemKhan <khanmonaem07@gmail.com>
2023-08-14 16:01:18 +0600

    ok

--- MiniInventoryManagementSystem/MiniInventoryManagementSystem/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MiniInventoryManagementSystem.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
