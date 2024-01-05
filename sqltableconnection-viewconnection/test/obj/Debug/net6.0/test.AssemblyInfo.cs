1b4ee95002b1ab0cc9d8cef31cb4fbd376028c05
MonaemKhan <khanmonaem07@gmail.com>
2024-01-05 09:38:44 +0600

    ok

--- sqltableconnection-viewconnection/test/obj/Debug/net6.0/test.AssemblyInfo.cs
+++ sqltableconnection-viewconnection/test/obj/Debug/net6.0/test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b490ecf1f9a07c52ed5847c70c76cd06d7845044")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
